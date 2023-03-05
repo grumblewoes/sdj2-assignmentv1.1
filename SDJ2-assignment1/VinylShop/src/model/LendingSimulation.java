72f0f5448b07787295ed78e5e12b5ffb7f702ed0
Damian Trafiałek <62154820+THEWhiteArrow@users.noreply.github.com>
2023-03-05 19:01:50 +0100

    feat: updated the simulation

--- SDJ2-assignment1/VinylShop/src/model/LendingSimulation.java
+++ SDJ2-assignment1/VinylShop/src/model/LendingSimulation.java
@@ -4,35 +4,48 @@ import java.util.ArrayList;
 
 public class LendingSimulation
 {
-  VinylModel model;
-  public LendingSimulation(VinylModel model){
-    this.model = model;
+  public LendingSimulation(VinylModelManager model){
     
-    new Thread( () -> {
+    new Thread(() -> {
       String[] bidders = { "Anna", "Jakub", "Catarina","Julia","Damian" };
       while(true){
+        try
+        {
+          Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+          throw new RuntimeException(e);
+        }
+
 
         try{
           String name = bidders[(int)Math.floor(Math.random()*bidders.length)];
           ArrayList<Vinyl> list =  model.getAllVinyls();
           if(list.size()==0) break;
+
           Vinyl v =list.get((int)Math.floor(Math.random()*list.size()));
-
-
+//
+          String action = "";
           double r = Math.random();
-          if(r<0.2){
-            model.reserveVinyl(v.getTitle(),name);
+          if(r<0.3){
   //          reserve
-          }else if(r<0.5){
-            model.borrowVinyl(v.getTitle(),name);
+            action="reserve";
+            model.reserveVinyl(v.getTitle(),name);
+          }else if(r<0.6){
   //          borrow
+            action="borrow";
+            model.borrowVinyl(v.getTitle(),name);
           }else if(r< 0.9){
-            model.returnVinyl(v.getTitle());
   //          return
+            action="return";
+            model.returnVinyl(v.getTitle());
           }else{
   //          remove
+            action="remove";
             model.removeVinyl(v.getTitle());
           }
+          System.out.println("---simulation: { name:"+name+", vinyl: "+v.getTitle() +", action: "+action  + " }");
         }catch  (Exception e){
           System.out.println(e.getMessage());
         }
@@ -42,3 +55,4 @@ public class LendingSimulation
     
   }
 }
+
